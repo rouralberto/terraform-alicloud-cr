c91bf0c8ac0c9da34aafeee80d10023ffa6feeb3
Alberto Roura <mail@albertoroura.com>
2019-11-19 10:14:47 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "ecr_user_id" {
-  value = alicloud_ram_user.namespace_user.id
+  description = "The ECR Namespace's User ID"
+  value       = alicloud_ram_user.namespace_user.id
 }
 
 output "ecr_user_name" {
-  value = alicloud_ram_user.namespace_user.name
+  description = "The ECR Namespace's User Name"
+  value       = alicloud_ram_user.namespace_user.name
 }
 
 output "ecr_user_key" {
-  value = alicloud_ram_access_key.ecr_ak.id
+  description = "The ECR Namespace's User access key ID"
+  value       = alicloud_ram_access_key.ecr_ak.id
 }
