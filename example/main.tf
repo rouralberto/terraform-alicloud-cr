66d830cd580f8fcb338677cbe6f5a9a8c9b33252
Alberto Roura <mail@albertoroura.com>
2019-11-19 12:30:34 +1000

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,1 +1,11 @@
+provider "alicloud" {}
 
+variable "namespace" {
+  default = "my_ecr_namespace"
+}
+
+module "cr" {
+  source    = "roura356a/cr/alicloud"
+  version   = "1.0.6" 
+  namespace = var.namespace
+}
