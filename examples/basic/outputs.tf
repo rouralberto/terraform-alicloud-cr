dcc6a47be8b06c65247f3947def509da6ec2cfcd
Alberto Roura <albert@studionone.com.au>
2019-11-20 15:35:48 +1000

    improved example

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -1,9 +1,19 @@
 output "cr_access_key" {
-  description = "The CR Namespace's User access key ID"
+  description = "The CR Namespace's Access Key"
   value       = module.cr.cr_access_key
 }
 
 output "cr_endpoint" {
   description = "Public endpoint of the registry"
   value       = module.cr.cr_endpoint
+}
+
+output "repository_ids" {
+  description = "List of repository IDs created"
+  value       = module.cr.repository_ids
+}
+
+output "disposable_password" {
+  description = "Password to activate the console login profile, forces to reset it"
+  value       = module.cr.disposable_password
 }
