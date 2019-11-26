15632e879c605647660d07423847f8204eecf92f
Alberto Roura <albertoroura@gmail.com>
2019-11-26 18:26:43 +1000

    Formatted go test file

--- test/terraform_test.go
+++ test/terraform_test.go
@@ -10,8 +10,8 @@ import (
 func TestTerraformAlicloudCr(t *testing.T) {
 	t.Parallel()
 
-	// The name for the namespace, only required variable
-	varNamespace := "cr_repo_namespace"
+	// The name for the namespace is the only required variable
+	varNamespace         := "cr_repo_namespace"
 	varDefaultRepository := "default"
 
 	terraformOptions := &terraform.Options{
@@ -27,22 +27,22 @@ func TestTerraformAlicloudCr(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Reading the outputs from the cr module
-	actualNamespaceId := terraform.Output(t, terraformOptions, "cr_namespace")
-	actualNamespaceUser := terraform.Output(t, terraformOptions, "cr_user")
-	actualAkStatus := terraform.Output(t, terraformOptions, "access_key_status")
-	actualRepositoryIds := terraform.Output(t, terraformOptions, "repository_ids")
-	actualRamUser := terraform.Output(t, terraformOptions, "ram_user")
-	actualPolicyName := terraform.Output(t, terraformOptions, "ram_policy_name")
-	actualPolicyType := terraform.Output(t, terraformOptions, "ram_policy_type")
+	actualNamespaceId      := terraform.Output(t, terraformOptions, "cr_namespace")
+	actualNamespaceUser    := terraform.Output(t, terraformOptions, "cr_user")
+	actualAkStatus         := terraform.Output(t, terraformOptions, "access_key_status")
+	actualRepositoryIds    := terraform.Output(t, terraformOptions, "repository_ids")
+	actualRamUser          := terraform.Output(t, terraformOptions, "ram_user")
+	actualPolicyName       := terraform.Output(t, terraformOptions, "ram_policy_name")
+	actualPolicyType       := terraform.Output(t, terraformOptions, "ram_policy_type")
 	actualPolicyAttachment := terraform.Output(t, terraformOptions, "ram_policy_attachment")
 
 	// Expected outputs
-	expectedNamespaceId := varNamespace
-	expectedUserName := varNamespace + "-cr-user"
-	expectedPolicyName := varNamespace + "-cr-policy"
+	expectedNamespaceId      := varNamespace
+	expectedUserName         := varNamespace + "-cr-user"
+	expectedPolicyName       := varNamespace + "-cr-policy"
 	expectedPolicyAttachment := "user:" + actualPolicyName + ":" + actualPolicyType + ":" + actualRamUser
-	expectedRepositoryIds := "[\n  \"" + varNamespace + "/" + varDefaultRepository + "\",\n]"
-	expectedAkStatus := "Active"
+	expectedRepositoryIds    := "[\n  \"" + varNamespace + "/" + varDefaultRepository + "\",\n]"
+	expectedAkStatus         := "Active"
 
 	// Tests against expected values from created resources in the cr module
 	assert.Equal(t, expectedNamespaceId, actualNamespaceId)
